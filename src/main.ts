aaaccdfd85ab10ad05da664a0ecc1a7b5cd5dccf
Yusuf Eren <erenyusuf170@gmail.com>
2022-10-04 03:06:01 +0300

    main

--- src/main.ts
+++ src/main.ts
@@ -1,31 +1,5 @@
-import { Controller, Module, Get } from "@nestjs/common";
 import { NestFactory } from "@nestjs/core";
-
-// this decorator is telling Nest that
-// we are trying to create a class
-// that is going to serve as a controller
-// inside of our application
-@Controller()
-class AppController {
-    // @Get declares route of the app
-    @Get("/a")
-    getRootRoute() {
-        return "hi there!";
-    }
-}
-
-@Controller()
-class TestController {
-    @Get("/hello")
-    getRootRoute() {
-        return "Hello thereeee";
-    }
-}
-
-@Module({
-    controllers: [AppController, TestController],
-})
-class AppModule {}
+import { AppModule } from "./app.module";
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
