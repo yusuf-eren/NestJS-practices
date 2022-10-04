a238f9a61970b688df43948375355952fc7ccb89
Yusuf Eren <erenyusuf170@gmail.com>
2022-10-04 04:18:57 +0300

    DTO

--- messages/src/messages/messages.controller.ts
+++ messages/src/messages/messages.controller.ts
@@ -1,18 +1,18 @@
-import { Controller, Get, Post } from '@nestjs/common';
+import { Controller, Get, Post, Body, Param } from '@nestjs/common';
+import { CreateMessageDto } from './dtos/create-message.dto';
 
 // route '/message'
 @Controller('messages')
 export class MessagesController {
   @Get()
-  listMessages() {
-    
-  }
+  listMessages() {}
   @Post()
-  createMessage(){
-    
+  // Data Transfer Object (DTO)
+  // CreateMessageDto is a validator
+  // for the body object
+  createMessage(@Body() body: CreateMessageDto) {
+    console.log(body);
   }
   @Get('/:id')
-  getMessage(){
-    
-  }
+  getMessage(@Param('id') id: string) {}
 }
