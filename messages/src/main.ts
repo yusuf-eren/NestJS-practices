5f96a049756e6e88815d31a94b63529442947a05
Yusuf Eren <erenyusuf170@gmail.com>
2022-10-04 04:19:12 +0300

    globalpipe added

--- messages/src/main.ts
+++ messages/src/main.ts
@@ -1,8 +1,12 @@
 import { NestFactory } from '@nestjs/core';
+import { ValidationPipe } from '@nestjs/common';
 import { MessagesModule } from './messages/messages.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(MessagesModule);
+
+  // global pipes are applied to all controllers
+  app.useGlobalPipes(new ValidationPipe());
   await app.listen(3000);
 }
 bootstrap();
