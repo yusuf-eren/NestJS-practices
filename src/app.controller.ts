d20d5d8854122f4f2ea71870aaf12947c1c177d6
Yusuf Eren <erenyusuf170@gmail.com>
2022-10-04 03:05:54 +0300

    controller

---
+++ src/app.controller.ts
@@ -0,0 +1,22 @@
+import { Controller, Get } from "@nestjs/common";
+
+// this decorator is telling Nest that
+// we are trying to create a class
+// that is going to serve as a controller
+// inside of our application
+@Controller()
+export class AppController {
+    // @Get declares route of the app
+    @Get("/a")
+    getRootRoute() {
+        return "hi there!";
+    }
+}
+
+@Controller()
+export class TestController {
+    @Get("/hello")
+    getRootRoute() {
+        return "Hello thereeee";
+    }
+}
