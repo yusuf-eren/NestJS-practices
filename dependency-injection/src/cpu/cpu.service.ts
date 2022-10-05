ea2ad5980a57ad932f9647d616318e4a691ef7bf
yusuf-eren <erenyusuf170@gmail.com>
2022-10-05 04:29:27 +0300

    cpu service

--- dependency-injection/src/cpu/cpu.service.ts
+++ dependency-injection/src/cpu/cpu.service.ts
@@ -1,4 +1,13 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable } from "@nestjs/common";
+import { PowerService } from "src/power/power.service";
 
 @Injectable()
-export class CpuService {}
+export class CpuService {
+    constructor(private powerService: PowerService) {}
+
+    compute(a: number, b: number): number {
+        console.log("Drawing 10watts of power from Power Supply");
+        this.powerService.supplyPower(10);
+        return a + b;
+    }
+}
