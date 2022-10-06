7aae5ae37eebdd9c1e4a394ed5a817c67027c7a5
yusuf-eren <erenyusuf170@gmail.com>
2022-10-07 02:02:42 +0300

    forFeature(User)

--- car-pricing-api/src/users/users.module.ts
+++ car-pricing-api/src/users/users.module.ts
@@ -1,9 +1,14 @@
-import { Module } from '@nestjs/common';
-import { UsersController } from './users.controller';
-import { UsersService } from './users.service';
+import { Module } from "@nestjs/common";
+import { UsersController } from "./users.controller";
+import { UsersService } from "./users.service";
+import { TypeOrmModule } from "@nestjs/typeorm";
+import { User } from "./user.entity";
 
 @Module({
+  // forFeature is selects the given entity
+  // and creates a repository for us
+  imports: [TypeOrmModule.forFeature([User])],
   controllers: [UsersController],
-  providers: [UsersService]
+  providers: [UsersService],
 })
 export class UsersModule {}
