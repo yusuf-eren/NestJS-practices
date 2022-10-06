61d16bdcc4cd4d97f5cc46ae86a5fc38ceb065f1
yusuf-eren <erenyusuf170@gmail.com>
2022-10-07 02:18:40 +0300

    little fix

--- car-pricing-api/src/users/users.module.ts
+++ car-pricing-api/src/users/users.module.ts
@@ -1,7 +1,7 @@
 import { Module } from "@nestjs/common";
 import { UsersController } from "./users.controller";
-import { UsersService } from "./users.service";
 import { TypeOrmModule } from "@nestjs/typeorm";
+import { UsersService } from "./users.service";
 import { User } from "./user.entity";
 
 @Module({
