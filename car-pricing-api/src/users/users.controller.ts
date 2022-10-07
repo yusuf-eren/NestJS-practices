ccbb477e6cdc18ea20f1ece5c2644a585f6c15a1
yusuf-eren <erenyusuf170@gmail.com>
2022-10-07 04:55:47 +0300

    creating user

--- car-pricing-api/src/users/users.controller.ts
+++ car-pricing-api/src/users/users.controller.ts
@@ -1,13 +1,16 @@
 import { Body, Controller, Post } from "@nestjs/common";
 import { CreateUserDto } from "./dtos/create-user.dto";
+import { UsersService } from "./users.service";
 
 @Controller("auth")
 export class UsersController {
+  constructor(private usersService: UsersService) {}
+
   @Post("/signup")
   // We are getting the body of the request
   // then declaring it as body and
   // declaring type of body as 'CreateUserDto'
   createUser(@Body() body: CreateUserDto) {
-    console.log(body);
+    this.usersService.create(body.email, body.password);
   }
 }
