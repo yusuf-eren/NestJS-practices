0b8ba8f48cef2438aa38d3366708dc277588ef15
yusuf-eren <erenyusuf170@gmail.com>
2022-10-07 03:14:47 +0300

    validationPipe added

--- car-pricing-api/src/main.ts
+++ car-pricing-api/src/main.ts
@@ -1,8 +1,17 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { NestFactory } from "@nestjs/core";
+import { ValidationPipe } from "@nestjs/common";
+import { AppModule } from "./app.module";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(
+    new ValidationPipe({
+      // if set to true, validator will strip
+      // validated object of any properties
+      // that do not use any validation decorators
+      whitelist: true,
+    }),
+  );
   await app.listen(3000);
 }
 bootstrap();
