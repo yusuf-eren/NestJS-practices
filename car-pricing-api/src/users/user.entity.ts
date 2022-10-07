c5c53a64f8f8c07e1d0859d586c00ef33d568d80
yusuf-eren <erenyusuf170@gmail.com>
2022-10-07 14:04:40 +0300

    user entity

--- car-pricing-api/src/users/user.entity.ts
+++ car-pricing-api/src/users/user.entity.ts
@@ -1,4 +1,11 @@
-import { Entity, Column, PrimaryGeneratedColumn } from "typeorm";
+import {
+  AfterInsert,
+  AfterRemove,
+  AfterUpdate,
+  Entity,
+  Column,
+  PrimaryGeneratedColumn,
+} from "typeorm";
 // all of these are decorators
 
 @Entity()
@@ -9,4 +16,24 @@ export class User {
   email: string;
   @Column()
   password: string;
+
+  // whenever we inster a new user into our database
+  // this functionright here should be executed
+  // NOTE : If we insert, update, or remove to db
+  // without creating an instance, Hooks are not going to execute
+  // that means we can not log insert, update, and remove.
+  @AfterInsert()
+  logInsert() {
+    console.log(`Inserted User with id ${this.id}`);
+  }
+
+  @AfterUpdate()
+  logUpdate(){
+    console.log(`Updated User with id ${this.id}`);
+  }
+
+  @AfterRemove()
+  logRemove(){
+    console.log(`Remove User with id ${this.id}`);
+  }
 }
