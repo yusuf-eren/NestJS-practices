e0f805ff225c933bebd9abffa3daa52333cdbf3b
yusuf-eren <erenyusuf170@gmail.com>
2022-10-08 01:33:48 +0300

    user routes and methods

--- car-pricing-api/src/users/users.controller.ts
+++ car-pricing-api/src/users/users.controller.ts
@@ -1,5 +1,15 @@
-import { Body, Controller, Post } from "@nestjs/common";
+import {
+  Body,
+  Controller,
+  Post,
+  Get,
+  Patch,
+  Param,
+  Query,
+  Delete,
+} from "@nestjs/common";
 import { CreateUserDto } from "./dtos/create-user.dto";
+import { UpdateUserDto } from "./dtos/update-user.dto";
 import { UsersService } from "./users.service";
 
 @Controller("auth")
@@ -12,5 +22,29 @@ export class UsersController {
   // declaring type of body as 'CreateUserDto'
   createUser(@Body() body: CreateUserDto) {
     this.usersService.create(body.email, body.password);
+  }
+
+  // We are getting the param of the request
+  // but don't pass it as 'string' !!!
+  // yes the param comes as a string
+  // so we should use parseInt() for param
+  @Get("/:id")
+  findUser(@Param("id") id: string) {
+    return this.usersService.findOne(parseInt(id));
+  }
+
+  @Get()
+  findAllUsers(@Query("email") email: string) {
+    return this.usersService.find(email);
+  }
+
+  @Delete("/:id")
+  deleteUser(@Param("id") id: string) {
+    return this.usersService.remove(parseInt(id));
+  }
+
+  @Patch("/:id")
+  updateUser(@Param("id") id: string, @Body() body: UpdateUserDto) {
+    return this.usersService.update(parseInt(id), body);
   }
 }
