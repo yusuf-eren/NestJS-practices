2597c637618c38854c3e74b7efbd70568bcb1a8a
yusuf-eren <erenyusuf170@gmail.com>
2022-10-08 02:07:16 +0300

    expection errors

--- car-pricing-api/src/users/users.service.ts
+++ car-pricing-api/src/users/users.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from "@nestjs/common";
+import { Injectable, NotFoundException } from "@nestjs/common";
 import { Repository } from "typeorm";
 import { InjectRepository } from "@nestjs/typeorm";
 import { User } from "./user.entity";
@@ -40,7 +40,7 @@ export class UsersService {
   // Partial is a type helper defined in TypeScript
   async update(id: number, attrs: Partial<User>) {
     const user = await this.findOne(id);
-    if (!user) throw new Error("user not found");
+    if (!user) throw new NotFoundException("user not found");
 
     // Object.assign method copies all enumerable
     // own properties from the given source(second element)
@@ -52,8 +52,8 @@ export class UsersService {
 
   async remove(id: number) {
     const user = await this.findOne(id);
-    if (!user) throw new Error("user not found");
-
+    if (!user) throw new NotFoundException("user not found");
+    
     return this.repo.remove(user);
   }
 }
