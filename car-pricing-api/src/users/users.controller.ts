1161a2fb8ffa67418273c4fac2a056264c1ca8d1
yusuf-eren <erenyusuf170@gmail.com>
2022-10-08 02:07:22 +0300

    expection

--- car-pricing-api/src/users/users.controller.ts
+++ car-pricing-api/src/users/users.controller.ts
@@ -7,6 +7,7 @@ import {
   Param,
   Query,
   Delete,
+  NotFoundException,
 } from "@nestjs/common";
 import { CreateUserDto } from "./dtos/create-user.dto";
 import { UpdateUserDto } from "./dtos/update-user.dto";
@@ -29,8 +30,11 @@ export class UsersController {
   // yes the param comes as a string
   // so we should use parseInt() for param
   @Get("/:id")
-  findUser(@Param("id") id: string) {
-    return this.usersService.findOne(parseInt(id));
+  async findUser(@Param("id") id: string) {
+    const user = await this.usersService.findOne(parseInt(id));
+    if (!user) throw new NotFoundException("user not found");
+
+    return user;
   }
 
   @Get()
