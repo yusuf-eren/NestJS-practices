5e90cb2dfe6f319ff6f5cabe3b7a37a263dedec0
yusuf-eren <erenyusuf170@gmail.com>
2022-10-07 19:03:02 +0300

    update, remove, findone, find methods created.

--- car-pricing-api/src/users/users.service.ts
+++ car-pricing-api/src/users/users.service.ts
@@ -23,4 +23,43 @@ export class UsersService {
     // it into our database
     return this.repo.save(user);
   }
+
+  // finds first user with given id
+  // (ids are unique but this is just an example)
+  findOne(id: number) {
+    return this.repo.findOneBy({ id });
+  }
+
+  // finds all the different users with
+  // the email passed to the function
+  find(email: string) {
+    return this.repo.find({ where: { email } });
+  }
+
+  // Partial<User> User is references to the User Entity
+  // Partial is a type helper defined in TypeScript
+  async update(id: number, attrs: Partial<User>) {
+    const user = await this.findOne(id);
+    if (!user) throw new Error("user not found");
+
+    // Object.assign method copies all enumerable
+    // own properties from the given source(second element)
+    // to target object(first element)
+    // NOTE: It modifies user object
+    Object.assign(user, attrs);
+    return this.repo.save(user);
+  }
+
+  async remove(id: number) {
+    const user = await this.findOne(id);
+    if (!user) throw new Error("user not found");
+
+    return this.repo.remove(user);
+  }
 }
+
+// const userService = new UsersService({} as any);
+// userService.update(1, {
+//   email: "asdsdsaasdas@gmail.com",
+//   password: "updatedPassword",
+// });
