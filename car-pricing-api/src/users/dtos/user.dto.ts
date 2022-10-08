dcfd7105cba254c15be3a6abdbdcba46520bdec0
yusuf-eren <erenyusuf170@gmail.com>
2022-10-09 01:13:20 +0300

    dto update

--- car-pricing-api/src/users/dtos/user.dto.ts
+++ car-pricing-api/src/users/dtos/user.dto.ts
@@ -1,8 +1,8 @@
 import { Expose, Exclude } from "class-transformer";
 
 export class UserDto {
-  @Expose()
-  id: number;
-  @Expose()
-  email: string;
+    @Expose()
+    id: number;
+    @Expose()
+    email: string;
 }
