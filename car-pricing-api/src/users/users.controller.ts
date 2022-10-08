3a1403b2a1ce4b38db012688480956318398bbdc
yusuf-eren <erenyusuf170@gmail.com>
2022-10-09 01:13:35 +0300

    service added

--- car-pricing-api/src/users/users.controller.ts
+++ car-pricing-api/src/users/users.controller.ts
@@ -1,21 +1,22 @@
 import {
-  Body,
-  Controller,
-  Post,
-  Get,
-  Patch,
-  Param,
-  Query,
-  Delete,
-  NotFoundException,
-  UseInterceptors,
-  ClassSerializerInterceptor,
+    Body,
+    Controller,
+    Post,
+    Get,
+    Patch,
+    Param,
+    Query,
+    Delete,
+    NotFoundException,
+    UseInterceptors,
+    ClassSerializerInterceptor,
 } from "@nestjs/common";
 import { CreateUserDto } from "./dtos/create-user.dto";
 import { UpdateUserDto } from "./dtos/update-user.dto";
 import { UsersService } from "./users.service";
 import { Serialize } from "src/interceptors/serialize.interceptor";
 import { UserDto } from "./dtos/user.dto";
+import { AuthService } from "./auth.service";
 
 @Controller("auth")
 // @Serialize() applies interceptor to all controllers under this route
@@ -23,43 +24,46 @@ import { UserDto } from "./dtos/user.dto";
 // as an example: @Serialize(UpdateUserDto)
 @Serialize(UserDto)
 export class UsersController {
-  constructor(private usersService: UsersService) {}
+    constructor(
+        private usersService: UsersService,
+        private authService: AuthService,
+    ) {}
 
-  @Post("/signup")
-  // We are getting the body of the request
-  // then declaring it as body and
-  // declaring type of body as 'CreateUserDto'
-  createUser(@Body() body: CreateUserDto) {
-    this.usersService.create(body.email, body.password);
-  }
+    @Post("/signup")
+    // We are getting the body of the request
+    // then declaring it as body and
+    // declaring type of body as 'CreateUserDto'
+    createUser(@Body() body: CreateUserDto) {
+        this.authService.signup(body.email, body.password);
+    }
 
-  // Interceptor is works like a middleware but it works before
-  // and after the controller.
-  // @Serialize(UserDto)
-  // We are getting the param of the request
-  // but don't pass it as 'string' !!!
-  // yes the param comes as a string
-  // so we should use parseInt() for param
-  @Get("/:id")
-  async findUser(@Param("id") id: string) {
-    const user = await this.usersService.findOne(parseInt(id));
-    if (!user) throw new NotFoundException("user not found");
+    // Interceptor is works like a middleware but it works before
+    // and after the controller.
+    // @Serialize(UserDto)
+    // We are getting the param of the request
+    // but don't pass it as 'string' !!!
+    // yes the param comes as a string
+    // so we should use parseInt() for param
+    @Get("/:id")
+    async findUser(@Param("id") id: string) {
+        const user = await this.usersService.findOne(parseInt(id));
+        if (!user) throw new NotFoundException("user not found");
 
-    return user;
-  }
+        return user;
+    }
 
-  @Get()
-  findAllUsers(@Query("email") email: string) {
-    return this.usersService.find(email);
-  }
+    @Get()
+    findAllUsers(@Query("email") email: string) {
+        return this.usersService.find(email);
+    }
 
-  @Delete("/:id")
-  deleteUser(@Param("id") id: string) {
-    return this.usersService.remove(parseInt(id));
-  }
+    @Delete("/:id")
+    deleteUser(@Param("id") id: string) {
+        return this.usersService.remove(parseInt(id));
+    }
 
-  @Patch("/:id")
-  updateUser(@Param("id") id: string, @Body() body: UpdateUserDto) {
-    return this.usersService.update(parseInt(id), body);
-  }
+    @Patch("/:id")
+    updateUser(@Param("id") id: string, @Body() body: UpdateUserDto) {
+        return this.usersService.update(parseInt(id), body);
+    }
 }
--- car-pricing-api/src/users/users.module.ts
+++ car-pricing-api/src/users/users.module.ts
@@ -3,12 +3,13 @@ import { UsersController } from "./users.controller";
 import { TypeOrmModule } from "@nestjs/typeorm";
 import { UsersService } from "./users.service";
 import { User } from "./user.entity";
+import { AuthService } from "./auth.service";
 
 @Module({
-  // forFeature is selects the given entity
-  // and creates a repository for us
-  imports: [TypeOrmModule.forFeature([User])],
-  controllers: [UsersController],
-  providers: [UsersService],
+    // forFeature is selects the given entity
+    // and creates a repository for us
+    imports: [TypeOrmModule.forFeature([User])],
+    controllers: [UsersController],
+    providers: [UsersService, AuthService],
 })
 export class UsersModule {}
