e3a1836c47bf532fdb7aae9dede6154f905ace8b
yusuf-eren <erenyusuf170@gmail.com>
2022-10-08 15:34:29 +0300

    serialization interceptor

---
+++ car-pricing-api/src/interceptors/serialize.interceptor.ts
@@ -0,0 +1,34 @@
+import {
+  UseInterceptors,
+  NestInterceptor,
+  ExecutionContext,
+  CallHandler,
+} from "@nestjs/common";
+
+import { Observable } from "rxjs";
+import { map } from "rxjs/operators";
+import { plainToClass } from "class-transformer";
+
+export function Serialize(dto: any) {
+  return UseInterceptors(new SerializeInterceptor(dto));
+}
+
+export class SerializeInterceptor implements NestInterceptor {
+  constructor(private dto: any) {}
+
+  intercept(context: ExecutionContext, handler: CallHandler): Observable<any> {
+    // Run something before a request is handled
+    // by the request handler
+    console.log("I'm running before te handler", context);
+
+    return handler.handle().pipe(
+      map((data: any) => {
+        return plainToClass(this.dto, data, {
+          // this setting ensures that whenever we have an
+          // instance of user and try to turn it into plain JSON
+          excludeExtraneousValues: true,
+        });
+      }),
+    );
+  }
+}
