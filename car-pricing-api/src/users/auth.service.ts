be467e2377ae407a99b9f27c948bfe395b72ece3
yusuf-eren <erenyusuf170@gmail.com>
2022-10-09 01:13:27 +0300

    signin and signup

---
+++ car-pricing-api/src/users/auth.service.ts
@@ -0,0 +1,38 @@
+import { Injectable, BadRequestException } from "@nestjs/common";
+import { UsersService } from "./users.service";
+import { randomBytes, scrypt as _scrypt } from "crypto";
+import { promisify } from "util";
+import { EntityNotFoundError } from "typeorm";
+
+const scrypt = promisify(_scrypt);
+
+@Injectable()
+export class AuthService {
+    constructor(private usersService: UsersService) {}
+
+    async signup(email: string, password: string) {
+        // See if email is in use
+        const users = await this.usersService.find(email);
+        if (users.length) throw new BadRequestException("email in use");
+        // Hash the users password
+        // Generate a salt
+        const salt = randomBytes(8).toString("hex");
+        // Hash the salt and the password together
+        const hash = (await scrypt(password, salt, 32)) as Buffer;
+        // Join the hashed result and salt together
+        const result = salt + "." + hash.toString("hex");
+        // Create a new user and save it
+        const user = await this.usersService.create(email, result);
+        // return user
+        return user;
+    }
+
+    async signin(email: string, password: string) {
+        const [user] = await this.usersService.find(email);
+        if (!user) return "not found";
+        const salt = randomBytes(8).toString("hex");
+        const hash = (await scrypt(password, salt, 32)) as Buffer;
+        const result = salt + "." + hash.toString("hex");
+        if (result === user.password) return "Succesfully Signed In";
+    }
+}
