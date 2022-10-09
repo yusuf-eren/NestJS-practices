ef6428646a9e60bbcf12efe039639b16b857298c
yusuf-eren <erenyusuf170@gmail.com>
2022-10-10 02:57:17 +0300

    test and little fix

--- car-pricing-api/src/users/users.controller.ts
+++ car-pricing-api/src/users/users.controller.ts
@@ -14,12 +14,12 @@ import {
 import { CreateUserDto } from "./dtos/create-user.dto";
 import { UpdateUserDto } from "./dtos/update-user.dto";
 import { UsersService } from "./users.service";
-import { Serialize } from "src/interceptors/serialize.interceptor";
+import { Serialize } from "../interceptors/serialize.interceptor";
 import { UserDto } from "./dtos/user.dto";
 import { AuthService } from "./auth.service";
 import { CurrentUser } from "./decorators/current-user.decorator";
 import { User } from "./user.entity";
-import { AuthGuard } from "src/guards/auth.guard";
+import { AuthGuard } from "../guards/auth.guard";
 
 @Controller("auth")
 // @Serialize() applies interceptor to all controllers under this route
