bd7cf8cc63472e0f5d3390168ad1dbd358eebbba
yusuf-eren <erenyusuf170@gmail.com>
2022-10-10 01:02:28 +0300

    auth jest tests

--- car-pricing-api/src/users/auth.service.spec.ts
+++ car-pricing-api/src/users/auth.service.spec.ts
@@ -3,25 +3,35 @@ import { AuthService } from "./auth.service";
 import { User } from "./user.entity";
 import { UsersService } from "./users.service";
 
-it("can create an instance of auth service", async () => {
-    // Create a fake copy of the users service
-    const fakeUsersService: Partial<UsersService> = {
-        find: () => Promise.resolve([]),
-        create: (email: string, password: string) =>
-            // -as User- means "threat this like User"
-            Promise.resolve({ id: 1, email, password } as User),
-    };
-    const module = await Test.createTestingModule({
-        providers: [
-            AuthService,
-            {
-                provide: UsersService,
-                useValue: fakeUsersService,
-            },
-        ],
-    }).compile();
+describe("AuthService", () => {
+    // we declared service type of AuthService because
+    // because first test is gonna throw an error
+    // if we don't set type
+    let service: AuthService;
 
-    const service = module.get(AuthService);
+    // This function runs before all tests
+    beforeEach(async () => {
+        // Create a fake copy of the users service
+        const fakeUsersService: Partial<UsersService> = {
+            find: () => Promise.resolve([]),
+            create: (email: string, password: string) =>
+                // -as User- means "threat this like User"
+                Promise.resolve({ id: 1, email, password } as User),
+        };
+        const module = await Test.createTestingModule({
+            providers: [
+                AuthService,
+                {
+                    provide: UsersService,
+                    useValue: fakeUsersService,
+                },
+            ],
+        }).compile();
 
-    expect(service).toBeDefined();
+        service = module.get(AuthService);
+    });
+
+    it("can create an instance of auth service", async () => {
+        expect(service).toBeDefined();
+    });
 });
