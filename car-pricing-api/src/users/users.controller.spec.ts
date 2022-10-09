0936b1fa6833be02bd4bd5785dbe253800672fdb
yusuf-eren <erenyusuf170@gmail.com>
2022-10-10 02:17:16 +0300

    user tests

--- car-pricing-api/src/users/users.controller.spec.ts
+++ car-pricing-api/src/users/users.controller.spec.ts
@@ -1,18 +1,21 @@
-import { Test, TestingModule } from '@nestjs/testing';
-import { UsersController } from './users.controller';
+import { Test, TestingModule } from "@nestjs/testing";
+import { AuthService } from "./auth.service";
+import { User } from "./user.entity";
+import { UsersController } from "./users.controller";
+import { UsersService } from "./users.service";
 
-describe('UsersController', () => {
-  let controller: UsersController;
+describe("UsersController", () => {
+    let controller: UsersController;
 
-  beforeEach(async () => {
-    const module: TestingModule = await Test.createTestingModule({
-      controllers: [UsersController],
-    }).compile();
+    beforeEach(async () => {
+        const module: TestingModule = await Test.createTestingModule({
+            controllers: [UsersController],
+        }).compile();
 
-    controller = module.get<UsersController>(UsersController);
-  });
+        controller = module.get<UsersController>(UsersController);
+    });
 
-  it('should be defined', () => {
-    expect(controller).toBeDefined();
-  });
+    it("should be defined", () => {
+        expect(controller).toBeDefined();
+    });
 });
