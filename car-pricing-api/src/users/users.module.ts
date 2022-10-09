ade40dcd5fe61c2792c2276ac7e585deb915d58a
yusuf-eren <erenyusuf170@gmail.com>
2022-10-09 05:19:32 +0300

    APP_INTERCEPTOR / Custom interceptor to handle ses

--- car-pricing-api/src/users/users.module.ts
+++ car-pricing-api/src/users/users.module.ts
@@ -4,12 +4,27 @@ import { TypeOrmModule } from "@nestjs/typeorm";
 import { UsersService } from "./users.service";
 import { User } from "./user.entity";
 import { AuthService } from "./auth.service";
+import { CurrentUserInterceptor } from "./interceptors/current-user.interceptor";
+import { APP_INTERCEPTOR } from "@nestjs/core";
 
 @Module({
     // forFeature is selects the given entity
     // and creates a repository for us
     imports: [TypeOrmModule.forFeature([User])],
     controllers: [UsersController],
-    providers: [UsersService, AuthService],
+    providers: [
+        UsersService,
+        AuthService,
+        // THIS IS A GLOBAL INTERCEPTOR FOR THIS CONTROLLER
+        // -----------------------------------------------
+        // This interceptor is getting session.userId and
+        // finding user by using findOne()
+        // then it adds it into request as "currentUser"
+        // --- request.currentUser ---
+        {
+            provide: APP_INTERCEPTOR,
+            useClass: CurrentUserInterceptor,
+        },
+    ],
 })
 export class UsersModule {}
