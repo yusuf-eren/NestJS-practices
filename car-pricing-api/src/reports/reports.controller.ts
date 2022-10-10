45fa04e1a07d97e88fbc3365e4edef9133a6bd67
yusuf-eren <erenyusuf170@gmail.com>
2022-10-11 00:23:28 +0300

    reports

--- car-pricing-api/src/reports/reports.controller.ts
+++ car-pricing-api/src/reports/reports.controller.ts
@@ -6,13 +6,15 @@ import {
     Patch,
     Param,
 } from "@nestjs/common";
-import { AuthGuard } from "src/guards/auth.guard";
+import { AuthGuard } from "../guards/auth.guard";
 import { CreateReportDto } from "./dtos/create-report.dto";
 import { ReportsService } from "./reports.service";
 import { CurrentUser } from "../users/decorators/current-user.decorator";
 import { User } from "../users/user.entity";
 import { reportDto } from "./dtos/report-dto";
 import { Serialize } from "../interceptors/serialize.interceptor";
+import { ApprovedReportDto } from "./dtos/approve-report.dto";
+import { AdminGuard } from "../guards/admin.guard";
 
 @Controller("reports")
 export class ReportsController {
@@ -23,5 +25,11 @@ export class ReportsController {
     @Serialize(reportDto)
     createReport(@Body() body: CreateReportDto, @CurrentUser() user: User) {
         return this.reportsService.create(body, user);
+    }
+
+    @Patch("/:id")
+    @UseGuards(AdminGuard)
+    approveReport(@Param("id") id: string, @Body() body: ApprovedReportDto) {
+        return this.reportsService.changeApproval(id, body.approved);
     }
 }
