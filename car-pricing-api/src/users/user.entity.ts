9fb3d79330ec4e3c4888836693e931183d3c579b
yusuf-eren <erenyusuf170@gmail.com>
2022-10-10 23:53:29 +0300

    user entity

--- car-pricing-api/src/users/user.entity.ts
+++ car-pricing-api/src/users/user.entity.ts
@@ -1,39 +1,43 @@
+import { Report } from "../reports/report.entity";
 import {
-  AfterInsert,
-  AfterRemove,
-  AfterUpdate,
-  Entity,
-  Column,
-  PrimaryGeneratedColumn,
+    AfterInsert,
+    AfterRemove,
+    AfterUpdate,
+    Entity,
+    Column,
+    PrimaryGeneratedColumn,
+    OneToMany,
 } from "typeorm";
 // all of these are decorators
 
 @Entity()
 export class User {
-  @PrimaryGeneratedColumn()
-  id: number;
-  @Column()
-  email: string;
-  @Column()
-  password: string;
+    @PrimaryGeneratedColumn()
+    id: number;
+    @Column()
+    email: string;
+    @Column()
+    password: string;
 
-  // whenever we inster a new user into our database
-  // this functionright here should be executed
-  // NOTE : If we insert, update, or remove to db
-  // without creating an instance, Hooks are not going to execute
-  // that means we can not log insert, update, and remove.
-  @AfterInsert()
-  logInsert() {
-    console.log(`Inserted User with id ${this.id}`);
-  }
+    @OneToMany(() => Report, (report) => report.user)
+    reports: Report[];
+    // whenever we inster a new user into our database
+    // this functionright here should be executed
+    // NOTE : If we insert, update, or remove to db
+    // without creating an instance, Hooks are not going to execute
+    // that means we can not log insert, update, and remove.
+    @AfterInsert()
+    logInsert() {
+        console.log(`Inserted User with id ${this.id}`);
+    }
 
-  @AfterUpdate()
-  logUpdate() {
-    console.log(`Updated User with id ${this.id}`);
-  }
+    @AfterUpdate()
+    logUpdate() {
+        console.log(`Updated User with id ${this.id}`);
+    }
 
-  @AfterRemove()
-  logRemove() {
-    console.log(`Remove User with id ${this.id}`);
-  }
+    @AfterRemove()
+    logRemove() {
+        console.log(`Remove User with id ${this.id}`);
+    }
 }
