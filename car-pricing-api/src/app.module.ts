20ab8ef3bb73596996a2d4f13ff1cf51f0064f20
yusuf-eren <erenyusuf170@gmail.com>
2022-10-10 04:38:58 +0300

    end to end test

--- car-pricing-api/src/app.module.ts
+++ car-pricing-api/src/app.module.ts
@@ -1,30 +1,59 @@
-import { Module } from "@nestjs/common";
+import { Module, ValidationPipe, MiddlewareConsumer } from "@nestjs/common";
+import { APP_PIPE } from "@nestjs/core";
 import { TypeOrmModule } from "@nestjs/typeorm";
 import { AppController } from "./app.controller";
 import { AppService } from "./app.service";
 import { UsersModule } from "./users/users.module";
 import { ReportsModule } from "./reports/reports.module";
+import { ConfigModule, ConfigService } from "@nestjs/config";
 // Entities
 import { User } from "./users/user.entity";
 import { Report } from "./reports/report.entity";
 
+const cookieSession = require("cookie-session");
+
 @Module({
-  imports: [
-    UsersModule,
-    ReportsModule,
-    TypeOrmModule.forRoot({
-      type: "sqlite", // db type
-      database: "db.sqlite", // filename
-      entities: [User, Report], // entities
-      synchronize: true, // *!*
-      // This option when set to true,
-      // is going to cause typeOrm to take a look
-      // at the structure of all your different entities
-      // and then automatically update the structure
-      // of your database
-    }),
-  ],
-  controllers: [AppController],
-  providers: [AppService],
+    imports: [
+      ConfigModule.forRoot({
+        isGlobal: true,
+        // NODE_ENV values
+        envFilePath: `.env${process.env.NODE_ENV}`
+      }),
+        UsersModule,
+        ReportsModule,
+        TypeOrmModule.forRoot({
+            type: "sqlite", // db type
+            database:
+                process.env.NODE_ENV === "test" ? "test.sqlite" : "db.sqlite", // filename
+            entities: [User, Report], // entities
+            synchronize: true, // *!*
+            // This option when set to true,
+            // is going to cause typeOrm to take a look
+            // at the structure of all your different entities
+            // and then automatically update the structure
+            // of your database
+        }),
+    ],
+    controllers: [AppController],
+    providers: [
+        AppService,
+        {
+            provide: APP_PIPE,
+            useValue: new ValidationPipe({
+                whitelist: true,
+            }),
+        },
+    ],
 })
-export class AppModule {}
+export class AppModule {
+    configure(consumer: MiddlewareConsumer) {
+        consumer
+            .apply(
+                cookieSession({
+                    // Secret key
+                    keys: ["this_is_my_cookie_string"],
+                }),
+            ) // this means apply to all routes
+            .forRoutes("*");
+    }
+}
