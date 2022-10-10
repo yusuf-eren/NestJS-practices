26e501d452f8fe2155b72a04601f86ade519dbb1
yusuf-eren <erenyusuf170@gmail.com>
2022-10-10 04:51:42 +0300

    test

--- car-pricing-api/src/users/users.service.spec.ts
+++ car-pricing-api/src/users/users.service.spec.ts
@@ -1,18 +1,54 @@
-import { Test, TestingModule } from '@nestjs/testing';
-import { UsersService } from './users.service';
+import { Test, TestingModule } from "@nestjs/testing";
+import { UsersController } from "./users.controller";
+import { UsersService } from "./users.service";
+import { AuthService } from "./auth.service";
+import { User } from "./user.entity";
 
-describe('UsersService', () => {
-  let service: UsersService;
+describe("UsersController", () => {
+    let controller: UsersController;
+    let fakeUsersService: Partial<UsersService>;
+    let fakeAuthService: Partial<AuthService>;
 
-  beforeEach(async () => {
-    const module: TestingModule = await Test.createTestingModule({
-      providers: [UsersService],
-    }).compile();
+    beforeEach(async () => {
+        fakeUsersService = {
+            findOne: (id: number) => {
+                return Promise.resolve({
+                    id,
+                    email: "asdf@asdf.com",
+                    password: "asdf",
+                } as User);
+            },
+            find: (email: string) => {
+                return Promise.resolve([
+                    { id: 1, email, password: "asdf" } as User,
+                ]);
+            },
+            // remove: () => {},
+            // update: () => {},
+        };
+        fakeAuthService = {
+            // signup: () => {},
+            // signin: () => {},
+        };
 
-    service = module.get<UsersService>(UsersService);
-  });
+        const module: TestingModule = await Test.createTestingModule({
+            controllers: [UsersController],
+            providers: [
+                {
+                    provide: UsersService,
+                    useValue: fakeUsersService,
+                },
+                {
+                    provide: AuthService,
+                    useValue: fakeAuthService,
+                },
+            ],
+        }).compile();
 
-  it('should be defined', () => {
-    expect(service).toBeDefined();
-  });
+        controller = module.get<UsersController>(UsersController);
+    });
+
+    it("should be defined", () => {
+        expect(controller).toBeDefined();
+    });
 });
