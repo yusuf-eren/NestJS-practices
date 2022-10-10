269b33a51a1447fc3cf371eaafd62b427605a204
yusuf-eren <erenyusuf170@gmail.com>
2022-10-10 04:38:51 +0300

    global variables

--- car-pricing-api/src/main.ts
+++ car-pricing-api/src/main.ts
@@ -1,24 +1,8 @@
 import { NestFactory } from "@nestjs/core";
-import { ValidationPipe } from "@nestjs/common";
 import { AppModule } from "./app.module";
-const cookieSession = require("cookie-session");
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
-    app.use(
-        cookieSession({
-            // Secret key
-            keys: ["this_is_my_cookie_string"],
-        }),
-    );
-    app.useGlobalPipes(
-        new ValidationPipe({
-            // if set to true, validator will strip
-            // validated object of any properties
-            // that do not use any validation decorators
-            whitelist: true,
-        }),
-    );
     await app.listen(3000);
 }
 bootstrap();
