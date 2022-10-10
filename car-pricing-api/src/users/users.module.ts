578ee0b03c804a31aca43e9477795f939fe54409
yusuf-eren <erenyusuf170@gmail.com>
2022-10-11 00:48:12 +0300

    replacing interceptor with global middleware

--- car-pricing-api/src/users/users.module.ts
+++ car-pricing-api/src/users/users.module.ts
@@ -1,11 +1,12 @@
-import { Module } from "@nestjs/common";
+import { Module, MiddlewareConsumer } from "@nestjs/common";
 import { UsersController } from "./users.controller";
 import { TypeOrmModule } from "@nestjs/typeorm";
 import { UsersService } from "./users.service";
 import { User } from "./user.entity";
 import { AuthService } from "./auth.service";
-import { CurrentUserInterceptor } from "./interceptors/current-user.interceptor";
-import { APP_INTERCEPTOR } from "@nestjs/core";
+// import { CurrentUserInterceptor } from "./interceptors/current-user.interceptor";
+// import { APP_INTERCEPTOR } from "@nestjs/core";
+import { CurrentUserMiddleware } from "./middlewares/current-user.middleware";
 
 @Module({
     // forFeature is selects the given entity
@@ -21,10 +22,15 @@ import { APP_INTERCEPTOR } from "@nestjs/core";
         // finding user by using findOne()
         // then it adds it into request as "currentUser"
         // --- request.currentUser ---
-        {
-            provide: APP_INTERCEPTOR,
-            useClass: CurrentUserInterceptor,
-        },
+        // {
+        //     provide: APP_INTERCEPTOR,
+        //     useClass: CurrentUserInterceptor,
+        // },
     ],
 })
-export class UsersModule {}
+export class UsersModule {
+    // NOTE: THAT MIDDLEWARE DOES EXACTLY THE SAME WHAT INTERCEPTOR DOES!!!
+    configure(consumer: MiddlewareConsumer) {
+        consumer.apply(CurrentUserMiddleware).forRoutes("*");
+    }
+}
