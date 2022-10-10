ae7264760a69f6c29673b8320a22131b228af5c9
yusuf-eren <erenyusuf170@gmail.com>
2022-10-10 23:53:01 +0300

    controller param, approved

--- car-pricing-api/src/reports/reports.controller.ts
+++ car-pricing-api/src/reports/reports.controller.ts
@@ -1,4 +1,27 @@
-import { Controller } from '@nestjs/common';
+import {
+    Body,
+    Controller,
+    Post,
+    UseGuards,
+    Patch,
+    Param,
+} from "@nestjs/common";
+import { AuthGuard } from "src/guards/auth.guard";
+import { CreateReportDto } from "./dtos/create-report.dto";
+import { ReportsService } from "./reports.service";
+import { CurrentUser } from "../users/decorators/current-user.decorator";
+import { User } from "../users/user.entity";
+import { reportDto } from "./dtos/report-dto";
+import { Serialize } from "../interceptors/serialize.interceptor";
 
-@Controller('reports')
-export class ReportsController {}
+@Controller("reports")
+export class ReportsController {
+    constructor(private reportsService: ReportsService) {}
+
+    @Post()
+    @UseGuards(AuthGuard)
+    @Serialize(reportDto)
+    createReport(@Body() body: CreateReportDto, @CurrentUser() user: User) {
+        return this.reportsService.create(body, user);
+    }
+}
