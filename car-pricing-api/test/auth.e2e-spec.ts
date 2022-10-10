9feda3edf69b6f78121ea27502c3fbfa1bddb2b0
yusuf-eren <erenyusuf170@gmail.com>
2022-10-10 14:39:20 +0300

    end to end tests

--- car-pricing-api/test/auth.e2e-spec.ts
+++ car-pricing-api/test/auth.e2e-spec.ts
@@ -16,7 +16,7 @@ describe("Authentication System", () => {
     });
 
     it("handles a signup request", () => {
-        const email = 'asdlkjq433safasfasasfasfas221@akl.com';
+        const email = "asdlkjq433safasfasasfasfas221@akl.com";
         return request(app.getHttpServer())
             .post("/auth/signup")
             .send({ email, password: "Alllaaaaa" })
@@ -26,5 +26,23 @@ describe("Authentication System", () => {
                 expect(id).toBeDefined();
                 expect(email).toEqual(email);
             });
+    });
+
+    it("signup as a new user then get the currently logged in user", async () => {
+        const email = "asdf@asdf.com";
+
+        const res = await request(app.getHttpServer())
+            .post("/auth/signup")
+            .send({ email, password: "asdf" })
+            .expect(201);
+
+        const cookie = res.get("Set-Cookie");
+
+        const { body } = await request(app.getHttpServer())
+            .get("/auth/whoami")
+            .set("Cookie", cookie)
+            .expect(200);
+
+        expect(body.email).toEqual(email);
     });
 });
