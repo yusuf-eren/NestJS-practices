22b053b7e9d33ff07223aae0e695950b9f13d8da
yusuf-eren <erenyusuf170@gmail.com>
2022-10-11 03:03:41 +0300

    ormconfig

---
+++ car-pricing-api/ormconfig.js
@@ -0,0 +1,30 @@
+var dbConfig = {
+    synchronize: false,
+    migrations: ["migrations/*.js"],
+    cli: {
+        migrationsDir: "migrations",
+    },
+};
+
+switch (process.env.NODE_ENV) {
+    case "development":
+        Object.assign(dbConfig, {
+            type: "sqlite",
+            database: "db.sqlite",
+            entities: ["**/*.entity.js"],
+        });
+        break;
+    case "test":
+        Object.assign(dbConfig, {
+            type: "sqlite",
+            database: "test.sqlite",
+            entities: ["**/*.entity.ts"],
+        });
+        break;
+    case "production":
+        break;
+    default:
+        throw new Error("unknown environment");
+}
+
+module.exports = dbConfig;
