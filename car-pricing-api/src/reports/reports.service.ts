ed4cd889d927f60c0b182c0befad9b180c640448
yusuf-eren <erenyusuf170@gmail.com>
2022-10-11 03:04:47 +0300

    service update

--- car-pricing-api/src/reports/reports.service.ts
+++ car-pricing-api/src/reports/reports.service.ts
@@ -4,6 +4,7 @@ import { Repository } from "typeorm";
 import { Report } from "./report.entity";
 import { CreateReportDto } from "./dtos/create-report.dto";
 import { User } from "../users/user.entity";
+import { GetEstimateDto } from "./dtos/get-estimate.dto";
 
 @Injectable()
 export class ReportsService {
@@ -20,10 +21,32 @@ export class ReportsService {
 
     async changeApproval(id: string, approved: boolean) {
         const report = await this.repo.findOne({ where: { id: parseInt(id) } });
-        console.log(report)
+        console.log(report);
         if (!report) throw new NotFoundException("report not found");
 
         report.approved = approved;
         return this.repo.save(report);
+    }
+
+    async estimatePrice({
+        make,
+        model,
+        lng,
+        lat,
+        mileage,
+        year,
+    }: GetEstimateDto) {
+        return await this.repo
+            .createQueryBuilder()
+            .select("*")
+            .where("make = :make", { make })
+            .andWhere("model = :model", { model })
+            .andWhere("lng - :lng BETWEEN -5 and 5", { lng })
+            .andWhere("lat - :lat BETWEEN -5 and 5", { lat })
+            .andWhere("year - :year BETWEEN -3 and 3", { year })
+            .orderBy("mileage - :mileage")
+            .setParameters({ mileage })
+            .limit(3)
+            .getRawMany();
     }
 }
